375274fdd58accc667e86ee8aecb05e3933875d4
mbholt <85723341+mbholt@users.noreply.github.com>
2022-05-01 22:38:46 -0400

    server.js, routes, npm i

---
+++ db/store.js
@@ -0,0 +1,52 @@
+const util = require("util");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+const { error } = require("console");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile)
+
+class Store {
+    read(){
+        return readFileAsync("../db/db.json", "utf8")
+    }
+
+    write(note){
+        return writeFileAsync("../db/db.json", JSON.stringify(note))
+    }
+
+    getNote(){
+        return this.read().then((notes)=>{
+            let parsedNotes;
+
+            try {
+                parsedNotes = [].concat(JSON.parse(notes))
+            } catch (err) {
+                parsedNotes = []
+            }
+
+            return parsedNotes
+        })
+    }
+
+    addNote(note){
+        const {title, text} = note;
+
+        if (!title || !text) {
+            throw new Error ("Please enter both text and title")
+        }
+        const newNote = {title, text, id: uuidv4()}
+
+        return this.getNote()
+            .then((notes)=> [...notes, newNote])
+            .then((updatedNotes)=> this.write(updatedNotes))
+            .then(()=> newNote)
+    }
+
+    deleteNote(id){
+        return this.getNote()
+            .then((notes)=> notes.filter((note)=> note.id !== id))
+            .then((filteredNotes)=> this.write(filteredNotes));
+    }
+}
+
+module.exports = new Store();
